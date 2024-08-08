375c9bdc3c746e3a9e49abec5006cd9f55041b8b
Mshu72 <m.shu.720@gmail.com>
2024-08-09 00:26:12 +0900

    second commit

--- app/javascript/article.js
+++ app/javascript/article.js
@@ -1,1 +1,14 @@
-console.log("JavaScriptが読み込まれました")
+import $ from "jquery";
+
+$(function(){
+  $(document).on("mouseover", "h1", function () {
+    $(this).css({ "color": "blue" });
+  }).on("mouseout", "h1", function () {
+    $(this).css({ "color": "" });
+  })
+});
+
+$(".btn").on("click", function(){
+  $(".btn").toggleClass("close-btn");
+  $(".menu").toggleClass("open-menu");
+});
