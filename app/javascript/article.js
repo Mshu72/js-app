ba87b2b28c12a2b23f58370dcba219bdeded15ba
Mshu72 <m.shu.720@gmail.com>
2024-08-10 23:41:07 +0900

    Update article.js

--- app/javascript/article.js
+++ app/javascript/article.js
@@ -12,3 +12,11 @@ $(".btn").on("click", function(){
   $(".btn").toggleClass("close-btn");
   $(".menu").toggleClass("open-menu");
 });
+
+let count = 0; //letで宣言した関数は再代入できる
+count = 1;
+count = 2;
+count = 3;
+
+// var キーワードでは、値の再代入がこの宇な変数を宣言できる
+var booktitle = "Javascript"
